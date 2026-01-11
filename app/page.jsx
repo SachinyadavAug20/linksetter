4c4d44f88b57147511ab5d74a233de5371403667
SachinyadavAug20 <samtagon777@gmail.com>
2026-01-11 22:07:29 +0530

    Initial commit

---
+++ app/page.jsx
@@ -0,0 +1,11 @@
+
+export default function Home() {
+  return (
+  <div>
+    <div className="w-full mx-auto text-2xl text-center bg-black text-white font-bold p-2">
+        Hello linksetter
+    </div>
+  </div>
+  
+  );
+}
